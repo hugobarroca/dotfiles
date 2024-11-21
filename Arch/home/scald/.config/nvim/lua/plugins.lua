75f78007991cd83127cf23628da77a221637d894
Hugo Barroca <hugobarroca98@gmail.com>
2024-11-21 17:40:18 +0000

    Added native LSP configs, and Svelte treesitter configs

--- Arch/home/scald/.config/nvim/lua/plugins.lua
+++ Arch/home/scald/.config/nvim/lua/plugins.lua
@@ -1,10 +1,33 @@
 return {
+    { 'neovim/nvim-lspconfig',
+        config = function()
+            local lspconfig = require('lspconfig')
+
+            -- Configure the Svelte language server
+            lspconfig.svelte.setup{
+                on_attach = function(client, bufnr)
+                    -- Your on_attach function here (e.g., key mappings)
+                    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+                    local opts = { noremap=true, silent=true }
+
+                    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+                    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+                    -- Add more key mappings as needed
+                end,
+                flags = {
+                    debounce_text_changes = 150,
+                },
+            }
+        end,
+    },
+	-- IMPORTANT! Most syntax highlighting actually comes from TreeSitter. 
+	-- Make sure to include all languages you need here!
 	{
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate',
 		config = function()
 			require 'nvim-treesitter.configs'.setup {
-				ensure_installed = { "javascript", "typescript" }, -- Add other languages if needed
+				ensure_installed = { "javascript", "typescript", "svelte", "css" }, -- Add other languages if needed
 				highlight = {
 					enable = true, -- Enable syntax highlighting
 				},
