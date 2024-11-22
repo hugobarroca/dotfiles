3bd1d962ecab2a0d487e9dd87c54f6a3efdee381
Hugo Barroca <hugobarroca98@gmail.com>
2024-11-22 11:57:23 +0000

    Added keybindings for LSP client.

--- Arch/home/scald/.config/nvim/lua/remap.lua
+++ Arch/home/scald/.config/nvim/lua/remap.lua
@@ -4,7 +4,14 @@ vim.keymap.set('n', '<Leader>a', ':vs<CR><C-w>l', { noremap = true, silent = tru
 vim.keymap.set('n', '<Leader>s', ':sp<CR><C-w>j', { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>o', ':RG!<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>e', ':Explore<CR>', { noremap = true, silent = true })
--- This keymaps need to be replaced with nvim's native LSP functions
+
+-- LSP Settings
+vim.keymap.set('n', '<Leader>.', ':lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>f', ':lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>t', ':lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>,', ':lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+
+-- Old COC Settings, shouldn't be needed anymore...
 -- vim.keymap.set('n', '<Leader>.', '<Plug>(coc-codeaction)', { noremap = true, silent = true })
 -- vim.keymap.set('n', '<Leader>t', '<Plug>(coc-rename)', { noremap = true, silent = true })
 -- vim.keymap.set('n', '<Leader>f', ':CocCommand editor.action.formatDocument<CR>', { noremap = true, silent = true })
